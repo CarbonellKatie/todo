656094675e7f4a7323f3f9fc49187f75016a40ad
CarbonellKatie <72585228+CarbonellKatie@users.noreply.github.com>
2022-01-06 22:16:27 -0600

    save to local storage & display upon refresh
    enable todo items to be saved to local storage & loaded upon refresh or revisit

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const todoList = document.querySelector('.todo-list');
 const filterOption = document.querySelector('.filter-todo');
 
 //Event Listeners
-
+//when the page loads, check if there are any todo items in local storage that need to be loaded by calling getTodos
+document.addEventListener('DOMContentLoaded', getTodos);
 //when setting an event listener in JS, don't put any parameters or ( ) after the function name
 // todoButton.addEventListener('click', addTodo, false);
 todoButton.addEventListener('click', addTodo);
@@ -17,6 +18,7 @@ todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
 //functions
 
+//create a Todo and add it to the todoDiv
 function addTodo(event){
     //prevent form from submitting automatically
     event.preventDefault();
@@ -30,6 +32,9 @@ function addTodo(event){
     newTodo.classList.add('todo-item');
     newTodo.innerText = todoInput.value;
     todoDiv.appendChild(newTodo);
+
+    //add Todo to local localStorage
+    saveLocalTodos(todoInput.value);
     
     //Check mark button for each element
     const completedButton = document.createElement('button');
@@ -44,7 +49,6 @@ function addTodo(event){
     todoDiv.appendChild(deleteButton);
 
     //div now has li and mark/delete buttons, this is a todo element
-
     todoList.appendChild(todoDiv); 
     
     //clear input value in todo input bar
@@ -105,3 +109,61 @@ function filterTodo(event){
     }
     });
 }
+// save To-Dos to local storage
+function saveLocalTodos(todo){
+    //check if we already have a todo in our local storage
+    let todos;
+    //if we do not already have a todos in local storage that is saving the list, make a new empty one that we will
+    //add this todo to
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }
+    //if we do have an array called todos in local storage already, parse it and add a new todo to the end
+    else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    //push a new todo to the array and reassign the array in storage to the updated version
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+}
+
+function getTodos(){
+    //todos will be an array of Strings containing the name of the todo (i.e: wash the car, walk the dog)
+    let todos;
+    if(localStorage.getItem('todos') === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    //recreate addTodo to display Todos from local storage
+    todos.forEach(function(todo){
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    // create li, text for each element
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    newTodo.innerText = todo;
+    todoDiv.appendChild(newTodo);
+    
+    //Check mark button for each element
+    const completedButton = document.createElement('button');
+    completedButton.classList.add('complete-btn');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    todoDiv.appendChild(completedButton);
+
+    //Delete button for each element
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(deleteButton);
+
+    //div now has li and mark/delete buttons, this is a todo element
+    todoList.appendChild(todoDiv); 
+
+    });
+
+    }
